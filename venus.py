31b55e26830ad4a3e811c68f1a4d79e451fea10a
Boukacem Younes <ybookacem@gmail.com>
2024-10-15 11:22:04 +0100

    implemented the syncing with neptune

--- venus.py
+++ venus.py
@@ -4,6 +4,7 @@ from omegaconf import OmegaConf
 
 action_help = """Action, can be:
 - init ; initiliazes the repo according to the venus structure
+- sync ; synchronizes the group tags and xp tags from the dbs locally to the confs and to neptune
 - ds ; to create a new dataset
 - dp ; to create a new dataprep
 - xg ; to create a new xperiment group
@@ -17,7 +18,6 @@ if __name__ == "__main__":
 	parser.add_argument("--tags", "-t", default = None, help = "list of space separated tags for the new artifact")
 	parser.add_argument("--datasetid", "-dsi", default = None, help = "id of the dataset to create the dataprep in")
 	parser.add_argument("--xpgroupid", "-xgi", default = None, help = "is of the xperiment group to create the xperiment in")
-
 	args = parser.parse_args()
 	action = args.action
 
@@ -50,35 +50,61 @@ if __name__ == "__main__":
 			"xpgroups" : {}
 		})
 		OmegaConf.save(cfg, "xperiments/xperiments.db")
+		# creating the corresponding cache file (will be used when syncing)
+		OmegaConf.save(cfg, ".venus/xperiments.db.cache")
 	
 
 	# synchronize the xperiments.db and xpgroup-<id>.db tags with xp-<xgi>-<xpi>.conf
 	# WARNING: This supposes that all tags modifications are done through the dbs and not directly in the confs, synchronizing will overwrite the confs
 	elif action == "sync":
-		
+		print("Importing neptune ...")
 		import neptune
 		neptune_db = OmegaConf.load(".venus/neptune.db")
 
 		xperiments_db = OmegaConf.load("xperiments/xperiments.db")
-		for xpgroup_id, xpgroup_tags in xperiments_db["xpgroups"].items():
-			print(xpgroup_id,":",xpgroup_tags)
-			xpgroup_db = OmegaConf.load(f"xperiments/{xpgroup_id}/{xpgroup_id}.db")
-			for xp_id, xp_tags in xpgroup_db["xps"].items():
-				xp_conf = OmegaConf.load(f"xperiments/{xpgroup_id}/{xp_id}/{xp_id}.conf")
-				xp_conf["xpgroup-tags"] = xpgroup_tags
-				xp_conf["xp-tags"] = xp_tags
-				OmegaConf.save(xp_conf, f"xperiments/{xpgroup_id}/{xp_id}/{xp_id}.conf")
-				
-				run = neptune.init_run(
-					project = neptune_db["project"],
-					api_token = neptune_db["api-token"],
-					custom_run_id = f"XG-{xpgroup_id.split('-')[1]}-XP-{xp_id.split('-')[2]}"
-				)
-				run["sys/tags"].clear()
-				run["sys/tags"].add(xp_tags.split(" "))
-				run["sys/group_tags"].add(xpgroup_tags.split(" "))
-				run.stop()
+		xperiments_db_cache = OmegaConf.load(".venus/xperiments.db.cache")
 
+		for xpgroup_id, xpgroup_tags in xperiments_db["xpgroups"].items():
+			print(f"\n\n\n=== SYNCING {xpgroup_id}... ===")
+			xpgroup_db = OmegaConf.load(f"xperiments/{xpgroup_id}/{xpgroup_id}.db")
+			xpgroup_db_cache = OmegaConf.load(f".venus/{xpgroup_id}.db.cache")
+			
+			if xpgroup_id not in xperiments_db_cache["xpgroups"]:
+				sync_xpgroup_tags = True
+			else:
+				sync_xpgroup_tags = xpgroup_tags != xperiments_db_cache["xpgroups"][xpgroup_id]
+			
+			for xp_id, xp_tags in xpgroup_db["xps"].items():
+				if xp_id not in xpgroup_db_cache["xps"]:
+					sync_xp_tags = True
+				else:
+					sync_xp_tags = xp_tags != xpgroup_db_cache["xps"][xp_id]
+				
+				if sync_xpgroup_tags or sync_xp_tags:
+					print("\n-------------------------------------------------------------------------------------")
+					print(f">>> SYNCING {xp_id} ...")
+					xp_conf = OmegaConf.load(f"xperiments/{xpgroup_id}/{xp_id}/{xp_id}.conf")
+					xp_conf["xpgroup-tags"] = xpgroup_tags
+					xp_conf["xp-tags"] = xp_tags
+					OmegaConf.save(xp_conf, f"xperiments/{xpgroup_id}/{xp_id}/{xp_id}.conf")
+					
+					run = neptune.init_run(
+						project = neptune_db["project"],
+						api_token = neptune_db["api-token"],
+						custom_run_id = f"XG-{xpgroup_id.split('-')[1]}-XP-{xp_id.split('-')[2]}"
+					)
+					run["sys/tags"].clear()
+					run["sys/tags"].add(xp_tags.split(" "))
+					run["sys/group_tags"].clear()
+					run["sys/group_tags"].add(xpgroup_tags.split(" "))
+					run.stop()
+					print(f">>> DONE WITH {xp_id}")
+					print("-------------------------------------------------------------------------------------")
+			
+			OmegaConf.save(xpgroup_db, f".venus/{xpgroup_id}.db.cache")
+			print(f"=== DONE    {xpgroup_id}    ===")
+		
+		OmegaConf.save(xperiments_db, ".venus/xperiments.db.cache")
 
 	## Creating a new dataset
 	elif action == "ds":
@@ -92,7 +118,7 @@ if __name__ == "__main__":
 		# creating the base folder boilerplate
 		DIR = f"datasets/dataset-{new_dataset_id}/"
 		os.makedirs(DIR)
-		with open(DIR+"readme.md", "w") as f:
+		with open(DIR+f"readme-ds-{new_dataset_id}.md", "w") as f:
 			f.write("# DESCRIPTION\n\n")
 			f.write("# OBTENTION\n\n")
 			f.write("# META-DATA\n\n")
@@ -126,12 +152,13 @@ if __name__ == "__main__":
 		cfg["datapreps"][f"dataprep-{dsi}-{new_dataprep_id}"] =  args.tags
 		OmegaConf.save(cfg, f"datasets/dataset-{dsi}/datapreps-{dsi}/datapreps-{dsi}.db")
 
+
 		# creating the base folder boilerplate
 		DIR = f"datasets/dataset-{dsi}/datapreps-{dsi}/dataprep-{dsi}-{new_dataprep_id}/"
 		
 		os.makedirs(DIR)
 		
-		with open(DIR+"readme.md", "w") as f:
+		with open(DIR+f"readme-dp-{dsi}-{new_dataprep_id}.md", "w") as f:
 			f.write("# DESCRIPTION\n\n")
 			f.write("# OBTENTION\n\n")
 			f.write("# META-DATA\n\n")
@@ -151,7 +178,7 @@ if __name__ == "__main__":
 		# creating the id for the new dataset
 		cfg = OmegaConf.load("xperiments/xperiments.db")
 		new_xpgroup_id = cfg["last-xpgroup-id"] = cfg["last-xpgroup-id"] + 1
-		cfg["xpgroups"][f"xpgroup-{new_xpgroup_id}"] = args.tags
+		cfg["xpgroups"][f"xpgroup-{new_xpgroup_id}"] = args.tags.strip(" ") + f" XG-{new_xpgroup_id}"
 		OmegaConf.save(cfg, "xperiments/xperiments.db")
 
 		# creating the base folder boilerplate
@@ -163,8 +190,10 @@ if __name__ == "__main__":
 			"xps": {}
 		})
 		OmegaConf.save(cfg, DIR+f"xpgroup-{new_xpgroup_id}.db")
-	
-	
+		# creating the corresponding cache file (will be used later for syncing)
+		OmegaConf.save(cfg, f".venus/xpgroup-{new_xpgroup_id}.db.cache")
+
+
 	## Creating a new xp
 	elif action == "xp":
 
@@ -181,7 +210,7 @@ if __name__ == "__main__":
 		# creating the xp-<id> folder boilerplate
 		DIR = f"xperiments/xpgroup-{xgi}/xp-{xgi}-{new_xp_id}/"
 		os.makedirs(DIR)
-		with open(DIR+"readme.md", "w") as f:
+		with open(DIR+f"readme-xp-{xgi}-{new_xp_id}.md", "w") as f:
 			f.write("# DESCRIPTION\n\n")
 			f.write("# OBTENTION\n\n")
 			f.write("# META-DATA\n\n")
@@ -195,18 +224,6 @@ if __name__ == "__main__":
 		})
 		OmegaConf.save(cfg, DIR+f"xp-{xgi}-{new_xp_id}.conf")
 
-		# creating the neptune run with xpgroup_tags and xp_tags
-		import neptune
-		neptune_db = OmegaConf.load(".venus/neptune.db")
-		run = neptune.init_run(
-			project = neptune_db["project"],
-			api_token = neptune_db["api-token"],
-			custom_run_id = f"XG-{xgi}-XP-{new_xp_id}",
-			tags = xp_tags.split(" "),
-		)
-		run["sys/group_tags"].clear()
-		run["sys/group_tags"].add(xpgroup_tags.split(" "))
-		run.stop()
 
 	## Error in the --action cli argument
 	else:
